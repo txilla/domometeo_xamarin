cca16c57d74f56090d18ac5706392fd8b8d7e4f9
Arantxa Escalera <aescalera@habitatsoft.com>
2018-03-27 15:11:23 +0200

    Automapper, and create AutoMapperConfig

--- DomoMeteoXamarin/DomoMeteoXamarin/App.xaml.cs
+++ DomoMeteoXamarin/DomoMeteoXamarin/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DomoMeteoXamarin.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace DomoMeteoXamarin
 		public App ()
 		{
 			InitializeComponent();
+            AutoMapperConfig.Configure();
 
             MainPage = new NavigationPage(new MainPage
             {
---
+++ DomoMeteoXamarin/DomoMeteoXamarin/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomoMeteoXamarin.Configuration
+{
+    public static class AutoMapperConfig
+    {
+        public static void Configure()
+        {
+
+        }
+    }
+}
